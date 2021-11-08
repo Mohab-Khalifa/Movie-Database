c1e947220907a736aa196025d74f1a562c04b410
Mohab Khalifa <khalifamohab97@outlook.com>
2021-11-08 20:51:48 +0000

    [MKP-1] add project application, application tests and application properties

---
+++ src/main/java/com/qa/movieproject/SpringMovieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.movieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/movieproject/SpringMovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.movieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
