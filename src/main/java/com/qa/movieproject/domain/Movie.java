e6b6972aae0d48657430c15eab4e1280539a526d
Mohab Khalifa <khalifamohab97@outlook.com>
2021-11-08 20:59:57 +0000

    [MKP-1] add movie and controller classes

---
+++ src/main/java/com/qa/movieproject/domain/Movie.java
@@ -0,0 +1,68 @@
+package com.qa.movieproject.domain;
+
+import javax.persistence.Entity;
+
+@Entity // tells the DB to create table with attributes as fields
+public class Movie {
+
+	// Attributes
+	private String title;
+	private Integer releaseYear;
+	private String genre;
+	private int runtime;
+
+	// Default Constructor
+	public Movie() {
+		super();
+	}
+
+	// Constructor
+	public Movie(String title, Integer releaseYear, String genre, int runtime, Integer id) {
+		super();
+		this.title = title;
+		this.releaseYear = releaseYear;
+		this.genre = genre;
+		this.runtime = runtime;
+	}
+
+	// Getters & Setters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(Integer releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	// ToString
+	@Override
+	public String toString() {
+		return "Movie [Title=" + title + ", releaseYear=" + releaseYear + ", Genre=" + genre + ", runtime=" + runtime
+				+ "]";
+	}
+
+}
